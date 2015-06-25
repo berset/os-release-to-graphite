ee05c8e6707c4ff6c607839f61b2112fa82a0d14
Berner Setterwall <berner.setterwall@campanja.com>
2015-06-25 20:28:13 +0200

    Make hostname workaround.

--- send.sh
+++ send.sh
@@ -2,8 +2,9 @@
 
 OS=$(grep ^ID /etc/os-release |awk -F= '{print $2}')
 RELEASE=$(grep VERSION_ID /etc/os-release |awk -F= '{print $2}')
+NAME=$(cat /the_hostname)
 
 while true; do
-    echo "${OS}.${HOSTNAME}.version.${RELEASE} 1 `date +%s`" | nc ${GRAPHITE} 2003;
+    echo "${OS}.${NAME}.version.${RELEASE} 1 `date +%s`" | nc ${GRAPHITE} 2003;
     sleep 10;
 done
